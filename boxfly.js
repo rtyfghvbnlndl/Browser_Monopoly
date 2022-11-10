054c5b33e0411b1bc4c22b761fac48e1d26dc3bd
rtyfghvbnlndl <rtyfghvbnlndl@gmail.com>
2022-11-11 00:04:11 +0800

    11月10日

--- boxfly.js
+++ boxfly.js
@@ -1,47 +1,35 @@
 //document.body.clientWidth
-
-//输入格子数，计算各边格子个数
-function calX(box){
-    var x=4,y,w=0
-    while(w<box){
-        w=0
-        y=Math.floor(x)
-        x1=x
-        y1=y
-        do{
-            w+=2*(x1+y1)-5
-            x1-=4
-            y1-=4
-        }while(x1>=7&&y1>=7)
-        ++x
-    //console.log(w,x-1,y)
-    }
-    return [x-1,y]
-
-}
 //保留两位小数，只舍不入
-function float2(x){
-    x=Math.floor(x*100)/100
+function float2(n,x){
+    x=Math.floor(x*10**n)/10**n
     return x
 }
-//计算css参数
-function calsize(x,y,wid){
-    pad=float2(2*wid/100)
-    bor=float2(0.2*wid/100)
-    perX=float2((wid-(pad+bor)*(2*x+1))/x)
-    perY=Math.floor(perX/4*3)
-    perf=wid/12
+//输入格子数，计算各边格子个数,计算css参数
+function calsize(box,wid,hei){
+    var x=4,y,w=0,pxY,perX
+    var pad=float2(4,1*wid/100)
+    var bor=float2(4,0.2*wid/100)
+    while(w<box){
+        w=0
+        perX=float2(5,(1-(pad/wid+bor/wid)*(2*x))/x)
+        pxY=perX*wid/4*3
+        y=Math.floor(hei/(pxY+(pad+bor)*2))
+        w=Object.getOwnPropertyNames(boxCodeNew(x,y,box)).length
+        ++x
+    //console.log(perX)
+    }
+    perf=wid/17
     var dict={
-        "pad":pad,
-        "bor":bor,
-        "perX":perX,
-        "perY":perY,
-        "perf":perf
+        x:x-1,
+        y:y,
+        pad:pad,
+        bor:bor,
+        perX:perX*100-0.02,
+        pxY:pxY,
+        perf:perf
     }
     return dict
 }
-
-
 //生成格子
 function setBox(x,y,dfw,boxCode,calsize){
     //console.log(x,y)
@@ -55,8 +43,8 @@ function setBox(x,y,dfw,boxCode,calsize){
                 boxList[`${a}_${b}`].content=dfw[num][2]
                 boxList[`${a}_${b}`].number=num
                 boxList[`${a}_${b}`].css={
-                    width:calsize.perX+'px',
-                    height:calsize.perY+'px',
+                    width:calsize.perX+'%',
+                    height:calsize.pxY+'px',
                     border:calsize.bor+'px'+' solid rgb(137, 216, 255)',
                     padding:calsize.pad+'px',
                     float:'left',
@@ -65,8 +53,8 @@ function setBox(x,y,dfw,boxCode,calsize){
             }
             else{
                 boxList[`${a}_${b}`].css={
-                    width:calsize.perX+'px',
-                    height:calsize.perY+'px',
+                    width:calsize.perX+'%',
+                    height:calsize.pxY+'px',
                     border:calsize.bor+'px'+' solid rgb(255, 255, 255)',
                     padding:calsize.pad+'px',
                     float:'left',
@@ -76,6 +64,7 @@ function setBox(x,y,dfw,boxCode,calsize){
             }
         }
     }
+    console.log(boxList)
     return boxList
 }
 //批量给css的dict赋值
@@ -86,7 +75,7 @@ function setcss(x,y,dict,ob){
         }
     }
 }
-//给格子编号
+//给格子编号 已经不用啦，但是感觉也是一种方向
 function boxCode(x,y,l){
     var boxCodes={}
     x1=x
@@ -127,12 +116,57 @@ function boxCode(x,y,l){
     }while(x1>=7&&y1>=7)
     return boxCodes
 }
+//新的boxcode
+function boxCodeNew(x0,y0,l){
+    var boxCodes={},n=1,x,y,nn
+    //定义向上下左右
+    function write(){
+        boxCodes[`${x}_${y}`]=n
+        //console.log(x,y,'write'+n)
+        ++n
+    }
+    function up(){
+        --y
+        write()
+    }
+    function down(){
+        ++y
+        write()
+    }
+    function left(){
+        --x
+        write()
+    }
+    function right(){
+        ++x
+        write()
+    }
+    //开始点
+    x=x0
+    y=y0
+    write()
+    //条件判断方向
+    //console.log(n,l)
+    var t=0
+    while(n<=l&&y0-2*t>=1+2*t){
+        do{
+            nn=n
+            if(y==y0-2*t&&x>1+2*t&&x<x0+3-2*t)left()
+            else if(x==1+2*t&&y>1+2*t&&y<y0+1-2*t)up()
+            else if(y==1+2*t&&x<x0-2*t&&x>0+2*t)right()
+            else if(x==x0-2*t&&y<y0-2-2*t&&y>0+2*t)down()
+            //console.log(nn,n)
+
+        }while(n<=l&&n!=nn)
+    ++t
+    }
+    return boxCodes
+}
 //调试
 //var dfw={1:["stay",0,111],2:["stay",0,222],3:["move",1,333],4:["stay",1,444],5:["stay",0,555],6:["stay",0,666],length:6}
-///var calX=calX(6)
 
-//var calsize=calsize(calX[0],calX[1],1024)
-//console.log(b)
+//var calsize=calsize(12,1024,1024)
+//console.log(calsize)
 
 //var boxCode=boxCode(calX[0],calX[1],dfw.length)
 //console.log(boxCode)
--- communication.js
+++ communication.js
@@ -2,14 +2,14 @@ function findKey(obj,value,compare=(a,b)=> a===b){
     return Object.keys(obj).find(k=>compare(obj[k],value))
 }
 function num2XY(current,boxCode,boxlist,pp){
-    console.log(current)
+    //console.log(current)
     if(current[1]!=1&&current.boxNum!=0){
         XY=findKey(boxCode,current.boxNum)
 
         if(pp!=''){
             boxlist[`${XY}`].css={
-                width:calsize.perX+'px',
-                height:calsize.perY+'px',
+                width:calsize.perX+'%',
+                height:calsize.pxY+'px',
                 border:calsize.bor+'px'+' solid rgb(137, 216, 255)',
                 padding:calsize.pad+'px',
                 float:'left',
@@ -19,8 +19,8 @@ function num2XY(current,boxCode,boxlist,pp){
         }
         else {
             boxlist[`${XY}`].css={
-                width:calsize.perX+'px',
-                height:calsize.perY+'px',
+                width:calsize.perX+'%',
+                height:calsize.pxY+'px',
                 border:calsize.bor+'px'+' solid rgb(137, 216, 255)',
                 padding:calsize.pad+'px',
                 float:'left',
